488ad40316ab19368d0ae514101561d08ef28bc1
Goutham <gouthamrs4u@gmail.com>
2022-08-31 22:38:18 +0530

    Design changes for Form

--- src/components/video-form.tsx
+++ src/components/video-form.tsx
@@ -1,15 +1,15 @@
-import { Field, Formik, FormikErrors } from 'formik';
 import { useEffect, useState } from 'react';
+import { Field, Formik, FormikErrors } from 'formik';
 import { MODE } from '../common/enums';
 import { Author, Category, VideoFormValues } from '../common/interfaces';
 import { getAuthors } from '../services/authors';
 import { getCategories } from '../services/categories';
 import { Button } from './button';
+import styles from './video-form.module.css';
 
 type VideoFormProps = {
   mode: MODE;
   editData?: VideoFormValues | null;
-
   onSubmit: (mode: MODE, values: VideoFormValues) => void;
   onCancel: () => void;
 };
@@ -48,45 +48,67 @@ export const VideoForm = ({ mode, editData = initialValues, onSubmit, onCancel }
       }}>
       {({ values, errors, touched, handleChange, handleSubmit, isSubmitting }) => {
         return (
-          <form onSubmit={handleSubmit}>
-            <Field name="name" onChange={handleChange} value={values.name} placeholder="Enter video name" />
-            {errors.name && touched.name && errors.name}
-            <Field
-              component="select"
-              id="author"
-              name="author"
-              value={values.author}
-              placeholder="Select author"
-              multiple={false}
-              onChange={handleChange}>
-              <option value={-1} key={-1}>
-                Select author
-              </option>
-              {authors.map(({ id, name }) => {
-                return (
-                  <option value={id} key={id}>
-                    {name}
-                  </option>
-                );
-              })}
-            </Field>
-            {errors.author && touched.author && errors.author}
-            <Field component="select" id="categories" name="categories" value={values.categories} multiple={true} onChange={handleChange}>
-              {categories.map(({ id, name }) => {
-                return (
-                  <option value={id} key={id}>
-                    {name}
-                  </option>
-                );
-              })}
-            </Field>
-            {errors.categories && touched.categories && errors.categories}
-            <Button primary type="submit" disabled={isSubmitting}>
-              {mode == MODE.ADD ? 'Save' : 'Update'}
-            </Button>
-            <Button type="button" onClick={onCancel}>
-              Cancel
-            </Button>
+          <form className={styles.formContainer} onSubmit={handleSubmit}>
+            <h1>
+              {mode === MODE.ADD ? 'Add' : 'Edit'} video{mode === MODE.EDIT && `: ${editData?.name}`}
+            </h1>
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Video name</label>
+              <Field className={styles.input} name="name" onChange={handleChange} value={values.name} />
+            </div>
+            <span className={styles.error}>{errors.name && touched.name && errors.name}</span>
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Video author</label>
+              <Field
+                className={styles.select}
+                component="select"
+                id="author"
+                name="author"
+                value={values.author}
+                placeholder="Select author"
+                multiple={false}
+                onChange={handleChange}>
+                <option value={-1} key={-1}>
+                  Select author
+                </option>
+                {authors.map(({ id, name }) => {
+                  return (
+                    <option value={id} key={id}>
+                      {name}
+                    </option>
+                  );
+                })}
+              </Field>
+            </div>
+            <span className={styles.error}>{errors.author && touched.author && errors.author}</span>
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Video categories</label>
+              <Field
+                className={styles.select}
+                component="select"
+                id="categories"
+                name="categories"
+                value={values.categories}
+                multiple={true}
+                onChange={handleChange}>
+                {categories.map(({ id, name }) => {
+                  return (
+                    <option value={id} key={id}>
+                      {name}
+                    </option>
+                  );
+                })}
+              </Field>
+            </div>
+            <span className={styles.error}>{errors.categories && touched.categories && errors.categories}</span>
+            <div className={styles.formFooter}>
+              <Button primary type="submit" disabled={isSubmitting}>
+                {mode == MODE.ADD ? 'Save' : 'Update'}
+              </Button>
+              <Button type="button" onClick={onCancel}>
+                Cancel
+              </Button>
+            </div>
           </form>
         );
       }}
